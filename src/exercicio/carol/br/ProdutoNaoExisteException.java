bd9d7b6df341f53017ad0c1b372a9c8877166264
CarolShiny <carolinecg431@gmail.com>
2022-01-31 20:55:06 -0300

    Create ProdutoNaoExisteException.java

---
+++ src/exercicio/carol/br/ProdutoNaoExisteException.java
@@ -0,0 +1,10 @@
+package exercicio.carol.br;
+
+public class ProdutoNaoExisteException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+    public ProdutoNaoExisteException (String msg){
+        super(msg);
+    }
+
+}
