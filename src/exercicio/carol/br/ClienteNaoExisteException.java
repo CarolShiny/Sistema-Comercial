bcc1c025e1e356c5e2162e34dd19f3856dc3bf42
CarolShiny <carolinecg431@gmail.com>
2022-01-31 20:54:29 -0300

    Create ClienteNaoExisteException.java

---
+++ src/exercicio/carol/br/ClienteNaoExisteException.java
@@ -0,0 +1,10 @@
+package exercicio.carol.br;
+
+public class ClienteNaoExisteException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+    public ClienteNaoExisteException (String msg){
+        super(msg);
+    }
+
+}
